813b29685bd06053bf23c8f3912d7a2c473ad9c4
rakeshkurakula <rakesh.2994@gmail.com>
2020-07-04 23:15:53 +0530

    Initial commi

---
+++ config/pages/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+    
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
